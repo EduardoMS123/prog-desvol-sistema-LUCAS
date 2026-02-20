bf05d3c2321a398493273d4c6fa7a699e8be757b
EduardoMS <eduardo.montanha.santos@escola.pr.gov.br>
2026-02-20 11:51:38 -0300

    Add files via upload

--- PHYTON LUCAS/adivinhacao/main.py
+++ PHYTON LUCAS/adivinhacao/main.py
@@ -2,25 +2,32 @@ print("-----------------------------")
 print("\nBem vindo ao jogo de ADIVINHA\n")
 print("-----------------------------")
 
+#definir a variavel
 n_secreto = 4123
-entrada = int(input("Digita um número ai mano! ->"))
-acertou = entrada == n_secreto
-entrada_maior = entrada > n_secreto
-entrada_menor = entrada < n_secreto
+n_tentativas = 5
+maximo_tentativa = 5
 
-print(f"Você digitou isso aqui -> {entrada}\n")
+for n_tentativas in range(1,n_tentativas + 1):
+    print(f"Você está na tentativa: {n_tentativas}/{maximo_tentativa}")
+    entrada = int(input("Digita um número ai mano!:"))
+    acertou = entrada == n_secreto
+    entrada_maior = entrada > n_secreto
+    entrada_menor = entrada < n_secreto
 
-#Verificação de acerto
-if(acertou):
-    print("ACERTOOOOOO\n") 
-else:
-    if(entrada_maior):
-       print("Errrou! Mas o número é maior doque o secreto\n")
-    if(entrada_menor):
-       print("Errrou! Mas o número é menor doque o secreto\n")
+    print(f"Você digitou isso aqui: {entrada}\n")
+
+    #Verificação de acerto
+    if(acertou):
+        print("ACERTOOOOOO\n")
+        break
+    else:
+        if(entrada_maior):
+            print("Errrou! Mas o número é maior doque o secreto\n")
+        if(entrada_menor):
+            print("Errrou! Mas o número é menor doque o secreto\n")
+    
 
 print("Cabou o games, valeuzão por jogar")
-
 
 
 
---
+++ PHYTON LUCAS/loopfor e while/loopFor.py
@@ -0,0 +1,11 @@
+
+print("--TAUBAUDA DOS NÚMEROS--")
+
+número = int(input("Coloque um número de 1 a 10:"))
+
+print(f"Tabuada do {número}")
+
+
+
+
+
