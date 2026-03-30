1e57dce93108104af806493c5af3381bf45528de
EduardoMS <eduardo.montanha.santos@escola.pr.gov.br>
2026-03-30 11:47:17 -0300

    Add files via upload

--- PHYTON LUCAS/jogos/forca.py
+++ PHYTON LUCAS/jogos/forca.py
@@ -5,24 +5,47 @@ def jogar_forca():
 
     lista = []
 
+    arquivo = open("jogos/palavras.txt", "r")
+    palavras = []
+
+    for linha in palavras:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
     palavra_secreta = "processador".upper()
-    letras_acertadas = ["_","_","_","_","_","_","_","_","_","_","_"]
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    #for letra in palavra_secreta
+        #letras_acertadas.append("_")
+
     perdeu = False
     acertou = False
+    erros = 0
 
     while not perdeu and not acertou:
         chute = input("Escreva uma letra: ")
-        chute = chute.strip()
-        index = 0
+        chute = chute.strip().upper()
 
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                letras_acertadas[index] = letra
-                print(letras_acertadas)   
-            index = index + 1
+        index = 0
+        if chute in palavra_secreta:
+
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                    print(letras_acertadas)
+                index += 1
+        else:
+            erros += 1
             
+        perdeu = erros == 6
+        acertou = "_" not in letras_acertadas
+
+        
+
             
-print()
+    
 
 
 
